95458665d6929aac5f79120e6d895a35ba02f93e
jens.meiners <jens.meiners@campus.tu-berlin.de>
2015-11-30 21:30:45 +0100

    minor changes for the reader

--- scripts/csv_reader.py
+++ scripts/csv_reader.py
@@ -3,4 +3,4 @@ import csv
 with open('output.cvs', 'rb') as out:
     reader = csv.reader(out, delimiter=',', quotechar='"')
     for i,line in enumerate(reader):
-        print i,'\t','- '.join(line)
+        print i,'\t','|'.join(line)
