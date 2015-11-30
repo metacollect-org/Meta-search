5ba46c9bf1dbd6eee7e99d320d623dcd1726bb1d
jens.meiners <jens.meiners@campus.tu-berlin.de>
2015-11-30 21:04:49 +0100

    add some scripts to download and parse the google spreadsheet

---
+++ scripts/csv_reader.py
@@ -0,0 +1,6 @@
+import csv
+
+with open('output.cvs', 'rb') as out:
+    reader = csv.reader(out, delimiter=',', quotechar='"')
+    for i,line in enumerate(reader):
+        print i,'\t','- '.join(line)
