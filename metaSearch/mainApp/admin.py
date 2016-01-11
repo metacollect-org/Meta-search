758964e6b09fef0fa890acfb768a5209248038ef
dandybudach <dandy.budach@gmail.com>
2016-01-11 22:24:24 +0100

    changed logo from file upload to URL; added category model

--- metaSearch/mainApp/admin.py
+++ metaSearch/mainApp/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 
 from .models import Project
+from .models import Category
 # Register your models here.
 
 class ProjectAdmin(admin.ModelAdmin):
     list_display = ('title', 'url', 'is_online')
 
 admin.site.register(Project, ProjectAdmin)
+admin.site.register(Category)
--- metaSearch/mainApp/models.py
+++ metaSearch/mainApp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings
 
 STATUS = (
     (0, 'Inactive'),
@@ -22,19 +23,27 @@ PAGE_LANGUAGES = (
 )
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
 class Project(models.Model):
     title = models.CharField(max_length=200)
     url = models.URLField()
     kind = models.IntegerField(choices=KINDS) # multiple
-    organisation_name = models.CharField(max_length=200)
-    categories = models.IntegerField() # tag field; id references one or more categories;
+    organisation_name = models.CharField(max_length=200, null=True, blank=True)
+    #categories = models.IntegerField() # tag field; id references one or more categories;
+    categories = models.ManyToManyField(Category)
     description = models.TextField()
     area_country = models.CharField(max_length=200)
     area_state = models.CharField(max_length=200) # bundesland
     area_city = models.CharField(max_length=200)
     status = models.IntegerField(choices=STATUS)
-    logo = models.FileField(upload_to='uploads/') # maybe better to use URL
-    contact_socialmedia = models.URLField() # comma separated
+    #logo = models.FileField(upload_to='mainApp/'+settings.UPLOAD_PATH, null=True, blank=True) # maybe better to use URL
+    logo = models.URLField(null=True, blank=True)
+    contact_socialmedia = models.URLField(null=True, blank=True) # comma separated
     contact_telephone = models.CharField(max_length=200) # comma separated
     contact_address_street = models.CharField(max_length=200)
     contact_address_housenr = models.CharField(max_length=20)
@@ -42,8 +51,8 @@ class Project(models.Model):
     contact_address_city = models.CharField(max_length=200)
     contact_address_country = models.CharField(max_length=200)
     languages = models.IntegerField(choices=PAGE_LANGUAGES) # multiple
-    needs = models.CharField(max_length=200) # optional
-    programming_language = models.IntegerField(choices=PROGRAMMING_LANGUAGES) # multiple
+    needs = models.CharField(max_length=200, null=True, blank=True) # optional
+    programming_language = models.IntegerField(choices=PROGRAMMING_LANGUAGES, null=True, blank=True) # multiple
 
     def __str__(self):
         return self.title
