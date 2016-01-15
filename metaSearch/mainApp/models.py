0cf11f74cfcd06ee6861a20da258afb6d836e6b7
dandybudach <dandy.budach@gmail.com>
2016-01-15 15:07:10 +0100

    changed nullable values to empty string default, changed details page

--- metaSearch/mainApp/models.py
+++ metaSearch/mainApp/models.py
@@ -59,31 +59,38 @@ class Project(models.Model):
     title = models.CharField(max_length=200)
     url = models.URLField()
     kind = models.IntegerField(choices=KINDS) # multiple
-    organisation_name = models.CharField(max_length=200, null=True, blank=True)
+    organisation_name = models.CharField(max_length=200, blank=True, default='')
     categories = models.ManyToManyField(Category)
 
     description_de = models.TextField()
-    description_en = models.TextField(blank = True)
+    description_en = models.TextField(blank = True, default='')
 
-
-    area_country = models.CharField(max_length=200, null=True, blank=True)
-    area_state = models.CharField(max_length=200, null=True, blank=True) # bundesland
-    area_city = models.CharField(max_length=200, null=True, blank=True)
+    area_country = models.CharField(max_length=200, blank=True, default='')
+    area_state = models.CharField(max_length=200, blank=True, default='') # bundesland
+    area_city = models.CharField(max_length=200, blank=True, default='')
     status = models.IntegerField(choices=STATUS)
     logo = models.URLField(null=True, blank=True, default=default_logo)
     contact_socialmedia = models.URLField(null=True, blank=True) # comma separated
-    contact_telephone = models.CharField(max_length=200, null=True, blank=True) # comma separated
-    contact_address_street = models.CharField(max_length=200, null=True, blank=True)
-    contact_address_housenr = models.CharField(max_length=20, null=True, blank=True)
-    contact_address_zip = models.CharField(max_length=20, null=True, blank=True)
-    contact_address_city = models.CharField(max_length=200, null=True, blank=True)
-    contact_address_country = models.CharField(max_length=200, null=True, blank=True)
+    contact_telephone = models.CharField(max_length=200, blank=True, default='') # comma separated
+    contact_address_street = models.CharField(max_length=200, blank=True, default='')
+    contact_address_housenr = models.CharField(max_length=20, blank=True, default='')
+    contact_address_zip = models.CharField(max_length=20, blank=True, default='')
+    contact_address_city = models.CharField(max_length=200, blank=True, default='')
+    contact_address_country = models.CharField(max_length=200, blank=True, default='')
     languages = models.ManyToManyField(PageLanguage) # multiple
-    needs = models.CharField(max_length=200, null=True, blank=True) # optional
+    needs = models.CharField(max_length=200, blank=True, default='') # optional
     programming_languages = models.ManyToManyField(ProgrammingLanguage, blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
+
+    def get_description(self, lang_code):
+        if not getattr(self, 'description_'+lang_code):
+            return getattr(self, 'description_de')
+        return getattr(self, 'description_'+lang_code)
 
     def is_online(self):
         return self.status==1
--- metaSearch/mainApp/views.py
+++ metaSearch/mainApp/views.py
@@ -1,4 +1,5 @@
 from django.http import Http404
+from django.utils import translation
 from django.shortcuts import render
 from haystack.query import SearchQuerySet
 from django.shortcuts import render_to_response
@@ -7,6 +8,8 @@ from haystack.forms import SearchForm
 from django.core.urlresolvers import reverse
 
 from .models import Project
+from .models import Category
+from .models import PageLanguage
 
 def index(request):
     project_list = SearchQuerySet().all()[:5]
@@ -19,10 +22,17 @@ def detail(request, project_id):
     print("ints")
     try:
         project = Project.objects.get(pk=project_id)
+
+        try:
+            lang_code = PageLanguage.objects.get(abbreviation=translation.get_language()).abbreviation
+        except PageLanguage.DoesNotExist:
+            lang_code = 'de'
+        desc = project.get_description(lang_code)
+
         print(project)
     except Project.DoesNotExist:
         raise Http404("Project does not exist")
-    return render(request, 'mainApp/detail.html', {'project': project})
+    return render(request, 'mainApp/detail.html', {'project': project, 'desc': desc})
 
 def search_titles(request):
     projects = SearchQuerySet().autocomplete(content_auto=request.GET.get('q', ''))
@@ -32,5 +42,4 @@ def search_titles(request):
 class ProjectNewView(generic.edit.CreateView):
     model = Project
     fields = [f.name for f in Project._meta.get_fields()]
-    #fields = ['title', 'languages']
     template_name = 'mainApp/new.html'
