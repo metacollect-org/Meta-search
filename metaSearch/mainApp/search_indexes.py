979cb000e684158f28413bff3b10e9b4927e8453
jens.meiners <jens.meiners@campus.tu-berlin.de>
2016-01-28 15:41:12 +0100

    FIXED geos usage

--- metaSearch/mainApp/search_indexes.py
+++ metaSearch/mainApp/search_indexes.py
@@ -1,6 +1,30 @@
 import datetime
+from django.utils import six
 from haystack import indexes
+from haystack.fields import LocationField
 from mainApp.models import Project
+
+
+class GeoField(LocationField):
+    field_type = 'location'
+
+    def prepare(self, obj):
+        return obj
+
+    def convert(self, value):
+        if value is None:
+            return None
+
+        if isinstance(value, six.string_types):
+            lat, lng = value.split(',')
+        elif isinstance(value, (list, tuple)):
+            # GeoJSON-alike
+            lat, lng = value[1], value[0]
+        elif isinstance(value, dict):
+            lat = value.get('lat', 0)
+            lng = value.get('lon', 0)
+
+        return "%s,%s" % (value.lat, value.lon)
 
 class ProjectIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
@@ -9,7 +33,7 @@ class ProjectIndex(indexes.SearchIndex, indexes.Indexable):
 
     content_auto = indexes.EdgeNgramField(model_attr='title')
     #pub_date = indexes.DateTimeField(model_attr='pub_date')
-    location = indexes.LocationField()
+    location = GeoField()
     geo_location_address = indexes.CharField(model_attr='get_location_address')
 
     def prepare_categoryName(self, object):
