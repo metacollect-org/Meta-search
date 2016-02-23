7ec9b57c8bf6e4fa35a56f450285f1ce595f5e0b
dandybudach <dandy.budach@gmail.com>
2016-02-23 17:07:01 +0100

    changes in style and front page added ajax search for titles and filter for categories (not final)

--- metaSearch/mainApp/static/mainApp/js/ajax.js
+++ metaSearch/mainApp/static/mainApp/js/ajax.js
@@ -1,19 +1,36 @@
 $(function(){
-/*
-    $('#search').keyup(function() {
 
+    $('#search').keyup(function() {
         $.ajax({
             type: "GET",
             url: "/mainApp/search/autocomplete/",
             data: {
-                'q' : $('#search').val()
+                'q' : $('#search').val(),
             },
             success: searchSuccess,
             dataType: 'html'
         });
 
     });
-*/
+
+     $('input[name=category]').on('click', function() {
+        var selected = []
+        $('input[name=category]:checked').each(function() {
+            selected.push($(this).val());
+        })
+        $.ajax({
+            type: "GET",
+            url: "/mainApp/search/autocomplete/",
+            data: {
+                'q' : $('#search').val(),
+                'category' : selected.join()
+            },
+            success: searchSuccess,
+            dataType: 'html'
+        });
+
+    });
+
 });
 
 function searchSuccess(data, textStatus, jqXHR)
