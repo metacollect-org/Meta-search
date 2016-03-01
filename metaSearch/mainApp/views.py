525884ed4119b8f732f8b71b22d5f39a9e49c3bb
dandybudach <dandy.budach@gmail.com>
2016-03-01 21:38:31 +0100

    added initial edit-page as a copy of the join form

--- metaSearch/mainApp/views.py
+++ metaSearch/mainApp/views.py
@@ -144,7 +144,11 @@ class ProjectDelete(generic.edit.DeleteView):
 
 class ProjectEdit(generic.edit.UpdateView):
     model = Project
-    fields = ['title']
+    fields = [] # Excluding updated_at and created_at as the property 'auto_now_add=True' makes them read-only, resulting in an error when adding them to this form.
+    for f in Project._meta.get_fields():
+        if f.name != 'updated_at' and f.name != 'created_at':
+            fields.append(f.name)
+
     template_name = 'mainApp/edit.html'
 
 #class ProjectDataView(generic.edit.CreateView):
